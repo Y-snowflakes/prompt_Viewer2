cefcee4c6ce05f1bcf89a39ac61816f0f6ffef34
Y-snowflakes <yuki.zoom@gmail.com>
2026-03-15 20:15:30 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, render_template, request
+from PIL import Image
+import piexif
+import piexif.helper
+
+app = Flask(__name__)
+
+
+def read_prompt(file):
+
+    im = Image.open(file)
+
+    if "parameters" in im.info:
+        return im.info["parameters"]
+
+    exif = im.info.get("exif")
+
+    if exif:
+
+        exif_dict = piexif.load(exif)
+
+        comment = exif_dict["Exif"].get(
+            piexif.ExifIFD.UserComment
+        )
+
+        if comment:
+            return piexif.helper.UserComment.load(comment)
+
+    return ""
+
+
+@app.route("/", methods=["GET","POST"])
+def index():
+
+    text = ""
+
+    if request.method == "POST":
+
+        file = request.files["file"]
+
+        text = read_prompt(file)
+
+    return render_template(
+        "index.html",
+        text=text
+    )
+
+
+if __name__ == "__main__":
+
+    port = int(os.environ.get("PORT",10000))
+
+    app.run(host="0.0.0.0",port=port)
